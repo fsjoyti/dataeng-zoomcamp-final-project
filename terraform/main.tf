421d1b32a25ea4c315330742df337b459b4f3a70
fsjoyti <fam211092@gmail.com>
2025-03-17 17:14:39 -0400

    initial

---
+++ terraform/main.tf
@@ -0,0 +1,46 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "6.16.0"
+    }
+  }
+}
+
+provider "google" {
+# Credentials only needs to be set if you do not have the GOOGLE_APPLICATION_CREDENTIALS set
+#  credentials = 
+  project = "vibrant-mantis-376307"
+  region  = "us-central1"
+}
+
+resource "google_storage_bucket" "data-lake-bucket" {
+  name          = "insurance-data-bucket-376307"
+  location      = "US"
+
+  # Optional, but recommended settings:
+  storage_class = "STANDARD"
+  uniform_bucket_level_access = true
+
+  versioning {
+    enabled     = true
+  }
+
+  lifecycle_rule {
+    action {
+      type = "Delete"
+    }
+    condition {
+      age = 30  // days
+    }
+  }
+
+  force_destroy = true
+}
+
+
+resource "google_bigquery_dataset" "dataset" {
+  dataset_id = "insurance_dataset_376307"
+  project    = "vibrant-mantis-376307"
+  location   = "US"
+}
